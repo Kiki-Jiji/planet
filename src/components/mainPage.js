a28ecdf1545d0a777d7b111f8343fc178375ebe3
Joshua Halls <joshuah40@googlemail.com>
2021-11-03 00:51:26 +0000

    working on frontend

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -4,39 +4,46 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+import AntDesign from './animation';
+
 class MainPage extends React.Component {
   
     render() {
       return(
-        <div>
+     
 
-        <div className="overlay">
-          <div className="intro">
-            <div className = "container">
-              <div className="row">
-                <h1>Commentary</h1>
-                <p>Easily write commentary in live markdown</p>
-              </div>
-            </div>
-          </div>
-        </div>
+            <Container fluid>
 
-        <Container className="features">
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
-          </Row>
-        </Container>
+        <AntDesign/>
 
-        <Container>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-          </Row>
-        </Container>
+           <section className="dark">
+      <h1>Nice Curves!</h1>
+      <p>A website is like a road. The more curves it has the more interesting it is.</p>
+    </section>
 
-        </div>
+              <section className="red">
+      <h1>Nice Curves!</h1>
+      <p>A website is like a road. The more curves it has the more interesting it is.</p>
+
+      <div class="wave">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+    
+    <div class="spacer layer1"></div>
+
+    <section className="purple">
+      <h1>Nice Curves!</h1>
+      <p>A website is like a road. The more curves it has the more interesting it is.</p>
+    </section>
+
+       
+    </Container>
       );
     }
   
